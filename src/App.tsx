269f3aeae02bd7f6ae500e5cee438d8baea7936e
Vivek Vellaiyappan <vivekvellaiyappans@gmail.com>
2024-12-15 21:15:11 -0600

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./App.css";
+
+function App() {
+  // start bilding your popup app here
+  return (
+    <h1 className="text-3xl text-center font-bold underline">CAG JS Example</h1>
+  );
+}
+
+export default App;
